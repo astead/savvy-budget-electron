636a00d4720038772526347124d5511e99aef288
Alan Stead <alan.stead@intel.com>
2023-11-14 16:39:30 -0800

    Add some more packages for building

--- public/main.js
+++ public/main.js
@@ -1,4 +1,6 @@
 const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev'); // To check if electron is in development mode
+const path = require('path');
 
 function createWindow () {
   // Create the browser window.
@@ -8,6 +10,12 @@ function createWindow () {
     title: "Savvy Budget",
     //frame: false,
     webPreferences: {
+      // The preload file where we will perform our app communication
+      preload: isDev 
+        ? path.join(app.getAppPath(), './public/preload.js') // Loading it from the public folder for dev
+        : path.join(app.getAppPath(), './build/preload.js'), // Loading it from the build folder for production
+      worldSafeExecuteJavaScript: true, // If you're using Electron 12+, this should be enabled by default and does not need to be added here.
+      contextIsolation: true, // Isolating context so our app is not exposed to random javascript executions making it safer.
       nodeIntegration: true,
       enableRemoteModule:true,
     }
@@ -16,15 +24,20 @@ function createWindow () {
   win.removeMenu()
   
   //load the index.html from a url
-  win.loadURL('http://localhost:3000');
-
-  // load the HTML file locally
-  //const winTwo = new BrowserWindow()
-  //winTwo.loadFile("public/index.html")
-
-  // Open the DevTools.
-  //win.webContents.openDevTools()
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000' // Loading localhost if dev mode
+      : `file://${path.join(__dirname, '../build/index.html')}` // Loading build file if in production
+  );  
 }
+
+// ((OPTIONAL)) Setting the location for the userdata folder created by an Electron app. It default to the AppData folder if you don't set it.
+app.setPath(
+  'userData',
+  isDev
+    ? path.join(app.getAppPath(), 'userdata/') // In development it creates the userdata folder where package.json is
+    : path.join(process.resourcesPath, 'userdata/') // In production it creates userdata folder in the resources folder
+);
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -38,7 +51,7 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
@@ -47,7 +60,15 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
-})
+});
+
+// Logging any exceptions
+process.on('uncaughtException', (error) => {
+  console.log(`Exception: ${error}`);
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
---
+++ public/preload.js
@@ -0,0 +1,12 @@
+//preload.js
+
+const { ipcRenderer, contextBridge } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+  // Invoke Methods
+  testInvoke: (args) => ipcRenderer.invoke('test-invoke', args),
+  // Send Methods
+  testSend: (args) => ipcRenderer.send('test-send', args),
+  // Receive Methods
+  testReceive: (callback) => ipcRenderer.on('test-receive', (event, data) => { callback(data) })
+});
