9be5556c373744684e97b05f4f6f2d0e188d5a5c
Alan Stead <alan.stead@intel.com>
2023-12-08 10:57:18 -0800

    Get dnd working for categories.

--- src/components/configure.tsx
+++ src/components/configure.tsx
@@ -4,7 +4,8 @@ import React, { useEffect, useState } from 'react';
 import { Header } from './header.tsx';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons"
-
+import { DragDropContext, Draggable } from "react-beautiful-dnd"
+import { StrictModeDroppable as Droppable } from '../helpers/StrictModeDroppable.js';
 
 export const Configure: React.FC = () => {
   
@@ -14,13 +15,24 @@ export const Configure: React.FC = () => {
   };
 
   const [newCategory, setNewCategory] = useState('');
-  const [categories, setCategories] = useState([{id:-1, category:''}]);
+  const [data, setData] = useState<any[]>([]);
+  const [categories, setCategories] = useState<any[]>(data || []);
 
   
   const handleDelete = (id) => {
     console.log('del category: ', id);
     
-    // Request we delete the category
+    const orderData = localStorage.getItem('categoryOrder');
+    if (orderData) {
+      const arrayIdsOrder = JSON.parse(orderData);
+      
+      if (arrayIdsOrder?.length) {
+        const newIdsOrderArray = arrayIdsOrder.filter(num => num !== id);
+        localStorage.setItem('categoryOrder', JSON.stringify(newIdsOrderArray));
+      }
+    }
+
+    // Request we delete the category in the DB
     const ipcRenderer = (window as any).ipcRenderer;
     ipcRenderer.send('del_category', id);
 
@@ -33,7 +45,7 @@ export const Configure: React.FC = () => {
   const handleSubmit = (e) => {
     e.preventDefault();  
     if (newCategory) {
-      const data = newCategory;  
+      const data = newCategory;
       console.log('new category: ', data);
         
       // Request we add the new category
@@ -45,6 +57,24 @@ export const Configure: React.FC = () => {
       
       // Query new data
     }
+  };
+
+  const handleOnDragEnd = (result) => {
+    if (!result?.destination) return;
+    
+    // This is where we re-order the categories in the database.
+    const cats = [...categories];
+    const [reorderedItem] = cats.splice(result.source.index, 1);
+    cats.splice(result.destination.index, 0, reorderedItem);
+
+    // Save this order in local storage.
+    // TODO: Need a better way to store this, if its something we 
+    // even want, since we'll have to re-sort the categories in
+    // several places.
+    const idsOrderArray = cats.map(cat => cat.id);
+    localStorage.setItem('categoryOrder', JSON.stringify(idsOrderArray));
+
+    setCategories(cats);
   };
 
   const newItemSection = (
@@ -67,19 +97,61 @@ export const Configure: React.FC = () => {
 
   let content;
   if (categories) {
-    content = categories.map((category) => {
-      return (
-        <article key={category.id}>
-          <div className="category">
-            <label>{category.category}</label>
-          </div>
-            <button className="trash" onClick={() => handleDelete( category.id )}>
-                <FontAwesomeIcon icon={faTrash} />
-            </button>
-        </article>
-      );
-    });
+    content = (
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId='categories'>
+          {(provided) => (
+            <section {...provided.droppableProps} ref={provided.innerRef}>
+              {categories.map((category, index) => {
+                return (
+                  <Draggable key={category.id} draggableId={category.id.toString()} index={index}>
+                    {(provided) => (
+                      <article {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                        <div className="category">
+                          <label>{category.category}</label>
+                        </div>
+                        <button className="trash" onClick={() => handleDelete( category.id )}>
+                            <FontAwesomeIcon icon={faTrash} />
+                        </button>
+                      </article>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </section>
+          )}
+        </Droppable>
+      </DragDropContext>
+    )
   }
+
+  useEffect(() => {
+    const orderData = localStorage.getItem('categoryOrder');
+    let arrayIdsOrder;
+
+    if (!orderData && data?.length) {
+      const idsOrderArray = data.map(category => category.id);
+      localStorage.setItem('categoryOrder', JSON.stringify(idsOrderArray));
+    }
+    if (orderData) {
+      arrayIdsOrder = JSON.parse(orderData);
+    }
+
+    let myArray;    
+    if (arrayIdsOrder?.length && data?.length) {
+      myArray = arrayIdsOrder.map(pos => {
+        return data.find(el => el.id === pos)
+      });
+
+      const newItems = data.filter(el => {
+        return !arrayIdsOrder.includes(el.id);
+      });
+      if (newItems?.length) myArray = [...newItems, ...myArray];
+    }
+
+    setCategories(myArray || data);
+  }, [data]);
 
   useEffect(() => {
     const ipcRenderer = (window as any).ipcRenderer;
@@ -92,7 +164,7 @@ export const Configure: React.FC = () => {
     ipcRenderer.on('list_data', (arg: CategoryDef[]) => {
       console.log('renderer: list_data2');
       console.log('arg:' + arg);
-      setCategories(arg);
+      setData(arg);
 
       ipcRenderer.removeAllListeners('list_data');
     });
