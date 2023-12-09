f7a05ab0c5e93864c74eab782758e3bd18dceb95
Alan Stead <alan.stead@intel.com>
2023-12-08 16:10:46 -0800

    Formatting.

--- public/db/db.js
+++ public/db/db.js
@@ -8,43 +8,42 @@ const dbPath = path.resolve(__dirname, './db.sqlite');
 
 // Create connection to SQLite database
 const knex = require('knex')({
-    client: 'sqlite3',
-    connection: {
-        filename: dbPath,
-    },
-    useNullAsDefault: true,
+  client: 'sqlite3',
+  connection: {
+    filename: dbPath,
+  },
+  useNullAsDefault: true,
 });
 
-
 function setup_db() {
-    // Create a table in the database called "books"
-    knex.schema
-        // Make sure no "books" table exists
-        // before trying to create new
-        .hasTable('category')
-            .then((exists) => {
-                if (!exists) {
-                    return knex.schema.createTable('category', (table)  => {
-                        table.increments('id').primary()
-                        table.integer('category')
-                    })
-                    .then(() => {
-                        // Log success message
-                        console.log('Table \'Category\' created')
-                    })
-                    .catch((error) => {
-                        console.error(`There was an error creating table: ${error}`)
-                    })
-                }
-            })
-            .then(() => {
-                console.log('done')
-            })
-            .catch((error) => {
-                console.error(`There was an error setting up the database: ${error}`)
-            })
+  // Create a table in the database called "books"
+  knex.schema
+    // Make sure no "books" table exists
+    // before trying to create new
+    .hasTable('category')
+    .then((exists) => {
+      if (!exists) {
+        return knex.schema
+          .createTable('category', (table) => {
+            table.increments('id').primary();
+            table.integer('category');
+          })
+          .then(() => {
+            // Log success message
+            console.log("Table 'Category' created");
+          })
+          .catch((error) => {
+            console.error(`There was an error creating table: ${error}`);
+          });
+      }
+    })
+    .then(() => {
+      console.log('done');
+    })
+    .catch((error) => {
+      console.error(`There was an error setting up the database: ${error}`);
+    });
 }
-
 
 // Export the database
 module.exports = knex;
