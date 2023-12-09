81a479024df60ed3e1e2fc6e2f1c62fbae54fc53
Alan Stead <alan.stead@intel.com>
2023-12-09 14:06:14 -0800

    Add header with months.

--- src/components/Envelopes.tsx
+++ src/components/Envelopes.tsx
@@ -1,9 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { Header } from './header.tsx';
 import { channels } from '../shared/constants.js'
+import { CompactTable } from '@table-library/react-table-library/compact';
+import { useTheme } from '@table-library/react-table-library/theme';
+import { getTheme } from '@table-library/react-table-library/baseline';
+
 
 export const Envelopes: React.FC = () => {
+  
+  const numMonths = 10;
+  const today = new Date();
+  const month = today.getMonth();
+  const year = today.getFullYear();
 
+  const arrayMonths = Array.from({length: numMonths}, (item, i) => {
+    const myDate = new Date(year, month+i);
+    const monthString = myDate.toLocaleString('en-US', {month: 'short'}) + "\n" + myDate.toLocaleString('en-US', {year: 'numeric'}) ;
+    return { 'label': monthString };
+  });
+
+  
+
+
+  
   
 
   return (
@@ -13,6 +32,17 @@ export const Envelopes: React.FC = () => {
       </header>
       <div>
         Envelopes<br/>
+        <article className="months-container">
+          {arrayMonths.map((myMonth, index) => {
+            return (
+              <div className="month-item">
+                {myMonth.label.toString()}
+              </div>
+            )
+          })}
+        </article>
+        <br/>
+        
       </div>
     </div>
   );
