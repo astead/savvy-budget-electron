e3d76a1288d1f0b02bd4c81d8a24a73535c20334
Alan Stead <alan.stead@intel.com>
2023-12-11 11:40:08 -0800

    Cleanup Configure page.

--- src/components/configure.tsx
+++ src/components/configure.tsx
@@ -15,6 +15,7 @@ import { channels } from '../shared/constants.js'
 export const Configure: React.FC = () => {
   
   const [data, setData] = useState<any[]>([]);
+  const [loaded, setLoaded] = useState(false);
 
   const groupBy = (data, key, label) => {
     return data.reduce(function(acc, item) {
@@ -55,10 +56,8 @@ export const Configure: React.FC = () => {
     const ipcRenderer = (window as any).ipcRenderer;
     ipcRenderer.send(channels.DEL_CATEGORY, id);
 
-    // Where do we move sub accounts in the deleted category?
+    // TODO: Where do we move sub accounts in the deleted category?
     // We should have an un-categorized section
-
-    // Query new data
   };
 
   const handleEnvDelete = (id) => {
@@ -68,10 +67,7 @@ export const Configure: React.FC = () => {
     const ipcRenderer = (window as any).ipcRenderer;
     ipcRenderer.send(channels.DEL_ENVELOPE, id);
 
-    // Where do we move sub accounts in the deleted category?
-    // We should have an un-categorized section
-
-    // Query new data
+    // TODO: Anything we need to clean up here?
   };
 
   const handleOnDragEnd = (result) => {
@@ -87,115 +83,81 @@ export const Configure: React.FC = () => {
       const ipcRenderer = (window as any).ipcRenderer;
       ipcRenderer.send(channels.MOV_ENVELOPE,  [result.draggableId, result.destination.droppableId] );
     }
-    
-    // This is where we re-order the categories in the database.
-    //const cats = [...categories];
-    //const [reorderedItem] = cats.splice(result.source.index, 1);
-    //cats.splice(result.destination.index, 0, reorderedItem);
-
-    // Save this order in local storage.
-    // TODO: Need a better way to store this, if its something we 
-    // even want, since we'll have to re-sort the categories in
-    // several places.
-    //const idsOrderArray = cats.map(cat => cat.id);
-    //localStorage.setItem('categoryOrder', JSON.stringify(idsOrderArray));
-
-    //setCategories(cats);
   };
 
   let content;
   if (data) {
     content = (
       <DragDropContext onDragEnd={handleOnDragEnd}>
-        
+        {data.map((category, index) => {
+          const { catID, cat:cat_name, items } = category;
           
-            
-              {data.map((category, index) => {
-                const { catID, cat:cat_name, items } = category;
-                //console.log("data:", category[0]);
-              
-                return (
-                  
-                  <Droppable droppableId={catID.toString()} key={"cat"+catID.toString()}>
-                    {(provided) => (
-                      <section  {...provided.droppableProps} ref={provided.innerRef}>
-                        <article className="category-container">
-                          <article className="category-item">
-                            <div className="category">
-                              <EditableCategory
-                                initialID={catID.toString()}
-                                initialName={cat_name} />
-                            </div>
-                            <NewEnvelope id={catID} />
-                            <button className="trash" onClick={() => handleDelete( catID )}>
-                                <FontAwesomeIcon icon={faTrash} />
-                            </button>
-                          </article>
-                          
-                          <article className="envelope-container">
-                          {
-                            items.map((env, index2) => {
-                              //console.log("env:", env);  
-                              return (
-                                (env.envID) &&
-                                <Draggable key={env.envID.toString()} draggableId={env.envID.toString()} index={index2}>
-                                  {(provided) => (
-                                    <article className="envelope-item-container" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                                      <article className="envelope-item">
-                                        <div className="envelope">
-                                          <EditableEnvelope
-                                            initialID={env.envID.toString()}
-                                            initialName={env.envelope} />
-                                        </div>
-                                        <button className="trash" onClick={() => handleEnvDelete( env.envID )}>
-                                            <FontAwesomeIcon icon={faTrash} />
-                                        </button>
-                                      </article>
-                                    </article>
-                                )}
-                                </Draggable>
-                              )
-                            })
-                          }
-                          </article>
-                        </article>
-                        { provided.placeholder }
-                      </section>
-                        
-                    )}
+          return (
+            <Droppable droppableId={catID.toString()} key={"cat"+catID.toString()}>
+              {(provided) => (
+                <section  {...provided.droppableProps} ref={provided.innerRef}>
+                  <article className="category-container">
+                    <article className="category-item">
+                      <div className="category">
+                        <EditableCategory
+                          initialID={catID.toString()}
+                          initialName={cat_name} />
+                      </div>
+                      <NewEnvelope id={catID} />
+                      <button className="trash" onClick={() => handleDelete( catID )}>
+                          <FontAwesomeIcon icon={faTrash} />
+                      </button>
+                    </article>
                     
-                  </Droppable>
-                );
-              })}
-              
-            
-         
-        
+                    <article className="envelope-container">
+                    {
+                      items.map((env, index2) => {
+                        //console.log("env:", env);  
+                        return (
+                          (env.envID) &&
+                          <Draggable key={env.envID.toString()} draggableId={env.envID.toString()} index={index2}>
+                            {(provided) => (
+                              <article className="envelope-item-container" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                                <article className="envelope-item">
+                                  <div className="envelope">
+                                    <EditableEnvelope
+                                      initialID={env.envID.toString()}
+                                      initialName={env.envelope} />
+                                  </div>
+                                  <button className="trash" onClick={() => handleEnvDelete( env.envID )}>
+                                      <FontAwesomeIcon icon={faTrash} />
+                                  </button>
+                                </article>
+                              </article>
+                          )}
+                          </Draggable>
+                        )
+                      })
+                    }
+                    </article>
+                  </article>
+                  { provided.placeholder }
+                </section>
+              )}
+            </Droppable>
+          );
+        })}   
       </DragDropContext>
     )
   }
 
-  
-
-  useEffect(() => {
-    const ipcRenderer = (window as any).ipcRenderer;
-
+  const load_cats_and_envs = () => {
     // Signal we want to get data
-    //console.log('Calling main:get_data');
+    const ipcRenderer = (window as any).ipcRenderer;
     ipcRenderer.send(channels.GET_CAT_ENV);
 
     // Receive the data
     ipcRenderer.on(channels.LIST_CAT_ENV, (arg) => {
-
-      //console.log('arg:', {...arg});
-
       const groupedData = groupBy(arg, 'catID', 'category');
-      //console.log('grouped:', groupedData);
-
       const sortedData = Object.values(groupedData).sort(compare);
-      //console.log('sorted:', sortedData);
 
       setData(sortedData);
+      setLoaded(true);
 
       ipcRenderer.removeAllListeners(channels.LIST_CAT_ENV);
     });
@@ -204,9 +166,13 @@ export const Configure: React.FC = () => {
     return () => {
       ipcRenderer.removeAllListeners(channels.LIST_CAT_ENV);
     };
+  }
 
+  useEffect(() => {
+    if (!loaded) {
+      load_cats_and_envs();
+    }
   }, []);
-
 
   return (
     <div className="App">
