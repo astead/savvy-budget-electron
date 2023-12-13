b43715c8634e5441d4398a9699706ab0b70b4978
Alan Stead <alan.stead@intel.com>
2023-12-12 23:49:22 -0800

    Forgot components for drop down and keywords!

---
+++ src/helpers/CategoryDropDown.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import 'react-edit-text/dist/index.css';
+import { channels } from '../shared/constants.js'
+
+export const CategoryDropDown = ({txID, envID, name, data}) => {
+  const [my_txID, ] = useState(txID);
+  const [my_envID, ] = useState(envID);
+  const [my_data, ] = useState(data);
+
+  const handleChange = (e) => {
+    console.log('handleChange: updating category ');
+    
+    // Request we update the DB
+    const ipcRenderer = (window as any).ipcRenderer;
+    ipcRenderer.send(channels.UPDATE_TX_ENV, [my_txID, e.target.value]);
+  };
+
+  return (
+    <select
+      name={my_txID}
+      value={envID}
+      onChange={handleChange}
+      className={"envelopeDropDown"+(envID === -1 ? "-undefined":"")}>
+        {data.map(o => (
+          <option key={o.envID} value={o.envID}>{o.category + " : " + o.envelope}</option>
+        ))}
+    </select>
+  );
+};
+
+export default CategoryDropDown;
---
+++ src/helpers/KeywordSave.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import 'react-edit-text/dist/index.css';
+import { channels } from '../shared/constants.js'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBookmark } from "@fortawesome/free-solid-svg-icons"
+
+export const KeywordSave = ({txID, envID, description, keywordEnvID}) => {
+  const [my_txID, ] = useState(txID);
+  const [my_envID, ] = useState(envID);
+  const [my_description, ] = useState(description);
+  const [my_keywordEnvID, ] = useState(keywordEnvID);
+
+  const saveKeyword = (e) => {
+    // Request we update the DB
+    const ipcRenderer = (window as any).ipcRenderer;
+    ipcRenderer.send(channels.SAVE_KEYWORD, [my_envID, my_description]);
+  };
+
+  return (
+    <div
+      onClick={saveKeyword}
+      className={"SaveKeyword" + (my_keywordEnvID === my_envID?"-saved":"")}>
+      <FontAwesomeIcon icon={faBookmark} />
+    </div>
+  );
+};
+
+export default KeywordSave;
