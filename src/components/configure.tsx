e52df49013244add2fa08839a0a825cab61bda0e
Alan Stead <alan.stead@intel.com>
2023-12-15 09:01:33 -0800

    Use tabs for different configure tasks.

--- src/components/configure.tsx
+++ src/components/configure.tsx
@@ -12,10 +12,54 @@ import EditableEnvelope from '../helpers/EditableEnvelope.tsx';
 import NewEnvelope from '../helpers/NewEnvelope.tsx';
 import { channels } from '../shared/constants.js';
 
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+
 export const Configure: React.FC = () => {
   
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
   const [data, setData] = useState<any[]>([]);
   const [loaded, setLoaded] = useState(false);
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   const groupBy = (data, key, label) => {
     return data.reduce(function(acc, item) {
@@ -204,8 +248,27 @@ export const Configure: React.FC = () => {
       </header>
       <div>
         Configure<br/>
-        <NewCategory/>        
-        {content}
+        
+        <Box sx={{ width: '100%' }}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs  value={value} onChange={handleChange}  aria-label="basic tabs example">
+              <Tab label="Categories" {...a11yProps(0)} />
+              <Tab label="Key Words" {...a11yProps(1)} />
+              <Tab label="Accounts" {...a11yProps(2)} />
+            </Tabs>
+          </Box>
+          <CustomTabPanel value={value} index={0}>
+            <NewCategory/>      
+            {content}
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            Item Two
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={2}>
+            Item Three
+          </CustomTabPanel>
+        </Box>
+        
       </div>
     </div>
   );
