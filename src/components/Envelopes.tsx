5e5ece8f17520d1e8b8d17ca0dd06199f9ea64c1
Alan Stead <alan.stead@intel.com>
2023-12-15 19:59:37 -0800

    Update TODO

--- src/components/Envelopes.tsx
+++ src/components/Envelopes.tsx
@@ -12,6 +12,7 @@ import Moment from 'moment';
   - Color based on how healthy the envelope is
   - Click on curr balance to move envelope balance option
   - option to show anything that isn't in a category
+  - Show current diff in budget on the right.
 */
 
 export const Envelopes: React.FC = () => {
