f924aae7a6959352d8df5f54cfb4a6230ffdcac2
Alan Stead <alan.stead@intel.com>
2023-12-18 13:18:36 -0800

    Save KW changes as data reloads.

--- src/helpers/KeywordSave.tsx
+++ src/helpers/KeywordSave.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'react-edit-text/dist/index.css';
 import { channels } from '../shared/constants.js'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -6,15 +6,27 @@ import { faBookmark } from "@fortawesome/free-solid-svg-icons"
 
 export const KeywordSave = ({txID, envID, description, keywordEnvID}) => {
   const [my_txID, ] = useState(txID);
-  const [my_envID, ] = useState(envID);
-  const [my_description, ] = useState(description);
-  const [my_keywordEnvID, ] = useState(keywordEnvID);
+  const [my_envID, setEnvID] = useState(envID);
+  const [my_description, setDescription] = useState(description);
+  const [my_keywordEnvID, setKeywordEnvID] = useState(keywordEnvID);
 
   const saveKeyword = (e) => {
     // Request we update the DB
     const ipcRenderer = (window as any).ipcRenderer;
     ipcRenderer.send(channels.SAVE_KEYWORD, [my_envID, my_description]);
   };
+
+  useEffect(() => {
+    setEnvID(envID);
+  }, [envID]);
+
+  useEffect(() => {
+    setDescription(description);
+  }, [description]);
+
+  useEffect(() => {
+    setKeywordEnvID(keywordEnvID);
+  }, [keywordEnvID]);
 
   return (
     <div
