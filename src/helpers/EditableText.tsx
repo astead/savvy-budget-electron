05826542570175ea8ec66d61954641ca05687565
Alan Stead <alan.stead@intel.com>
2024-01-09 19:02:30 -0800

    Add editableText

---
+++ src/helpers/EditableText.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { EditText } from 'react-edit-text';
+import 'react-edit-text/dist/index.css';
+
+export const EditableText = ({ initialID, initialName, callback, className, inputClassName}) => {
+  const [id, ] = useState(initialID);
+  const [name, setName] = useState(initialName);
+
+  return (
+    <EditText
+      name={id}
+      defaultValue={name}
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+      onBlur={callback(id, name)}
+      className={className}
+      inputClassName={inputClassName} />
+  );
+};
+
+export default EditableText;
